6340ea6559333f5d55688d2db4ea3f93971958bb
Hekla Rún Ámundadóttir <heklaruna@gmail.com>
2016-10-24 17:24:02 +0000

    löguðum smá í board

--- src/main/java/is/ru/tictactoe/Board.java
+++ src/main/java/is/ru/tictactoe/Board.java
@@ -7,8 +7,8 @@ public class Board {
 	 public Board() {
 		 board = new char[SIZE][SIZE];
 		 int i = 1;
-		 for( int row : SIZE){
-			 for( int col : SIZE){
+		 for( int row = 0; row < SIZE; row++){
+			 for( int col = 0; col < SIZE; col++){
 				 board[row][col] = i;
 				 i++;
 			 }
@@ -20,28 +20,28 @@ public class Board {
 		 int count = 0;
 		 
 		 //athugum larett
-		 for(int i : SIZE){
+		 for(int i = 0; i < SIZE; i++){
 			 if(board[i][col] == player)
 				 count++;
 		 }
 		 winner = win(count);
 		 
 		 //athugum lodrett
-		 for(int j : SIZE){
+		 for(int j = 0; j < SIZE; j++){
 			 if(board[row][j] == player)
 				 count++;
 		 }
 		 winner = win(count);
 		 
 		 //athugum a ska fra vinstri
-		 for(int i : SIZE){
+		 for(int i = 0; i < SIZE; i++){
 			 if(board[i][i] == player)
 				 count++;
 		 }
 		 winner = win(count);
 		 
 		 //athugum a ska fra haegri
-		 for(int j : SIZE){
+		 for(int j = 0; j < SIZE; j++){
 			 if(board[j][2-j] == player)
 				 count++;
 		 }
