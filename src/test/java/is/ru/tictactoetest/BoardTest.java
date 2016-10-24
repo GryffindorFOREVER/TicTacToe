b8c3e31fcb82ac47c0ec02704a469ff739949e06
Hekla Rún Ámundadóttir <heklaruna@gmail.com>
2016-10-24 11:50:25 +0000

    gerðum unit test til að testa winner fallið

--- src/test/java/is/ru/tictactoetest/BoardTest.java
+++ src/test/java/is/ru/tictactoetest/BoardTest.java
@@ -1,6 +1,62 @@
 package is.ru.tictactoe;
 
+import static org.junit.Assert.assertEquals;
+
 public class BoardTest {
 
-
+	@Test
+	public void XwinsDiagonalFromUpperLeft()
+	{
+		TicTacToe.Move(board[1][1], board[1][0], board[0][0], board[1][2], board[2][2]);
+	    Assert.Equals(Board.isWinner(row, col), true);
+	}
+	
+	@Test
+	public void OwinsDiagonalFromUpperLeft()
+	{
+		TicTacToe.Move(board[1][2], board[0][0], board[0][2], board[1][1], board[2][0], board[2][2]);
+	    Assert.Equals(Board.isWinner(row, col), true);
+	}
+	
+	@Test
+	public void XwinsDiagonalFromUpperRigth()
+	{
+		TicTacToe.Move(board[0][2], board[0][0], board[1][1], board[0][1], board[2][0]);
+	    Assert.Equals(Board.isWinner(row, col), true);
+	}
+	
+	@Test
+	public void OwinsDiagonalFromUpperRigth()
+	{
+		TicTacToe.Move(board[2][2], board[0][2], board[0][0], board[1][1], board[0][1], board[2][0]);
+	    Assert.Equals(Board.isWinner(row, col), true);
+	}
+	
+	@Test
+	public void XwinsVertical()
+	{
+		TicTacToe.Move(board[1][1], board[0][0], board[0][1], board[2][2], board[2][1]);
+	    Assert.Equals(Board.isWinner(row, col), true);
+	}
+	
+	@Test
+	public void OwinsVertical()
+	{
+		TicTacToe.Move(board[1][2], board[1][1], board[0][0], board[0][1], board[2][2], board[2][1]);
+	    Assert.Equals(Board.isWinner(row, col), true);
+	}
+	
+	@Test
+	public void XwinsHorizontal()
+	{
+		TicTacToe.Move(board[1][1], board[0][0], board[1][0], board[2][2], board[1][2]);
+	    Assert.Equals(Board.isWinner(row, col), true);
+	}
+	
+	@Test
+	public void OwinsHorizontal()
+	{
+		TicTacToe.Move(board[0][1], board[1][1], board[0][0], board[1][0], board[2][2], board[1][2]);
+	    Assert.Equals(Board.isWinner(row, col), true);
+	}
 }
