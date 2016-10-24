3c11f4eac332cc526ab40cde34baeecd0683411d
Kristin Vala Einarsdottir <kristine15@ru.is>
2016-10-24 18:16:57 +0000

    sma breytingar i board klasanum

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -2,14 +2,27 @@ package is.ru.tictactoe;
 
 public class TicTacToe {
 
-	private String yolo = "YOLO";
-	
-	public String YOLO() {
-
-		return yolo;
-	}
-
 	public static void main(String[] args) {
-		System.out.println("YOLO");
+		int counter = 0;
+
+		Board b = new Board();
+
+		while(!b.full()){
+			b.display();
+
+			if(isWinner()){
+				
+			}else if(){
+
+			}
+
+			StdOut.print("Draw!")
+
+
+		}
+
+
+
+
 	}	
 }
