749c9fb31d7d4d4e2935186f849f831bfd64bfa2
Hekla Rún Ámundadóttir <heklaruna@gmail.com>
2016-10-24 10:26:07 +0000

    bjuggum til tictactoeboard

--- src/main/java/is/ru/tictactoe/Board.java
+++ src/main/java/is/ru/tictactoe/Board.java
@@ -1,5 +1,18 @@
 package is.ru.tictactoe;
 
 public class Board {
-	
+	 private char[][] board;
+	 private final int SIZE = 3;
+	 
+	 public Board() {
+		 board = new char[SIZE][SIZE];
+		 int i = 1
+		 for( int row : SIZE){
+			 for( int col : SIZE){
+				 board[row][col] = i;
+				 i++;
+			 }
+		 }
+	 }
 }
+
