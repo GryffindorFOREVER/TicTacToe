4cbbe2a59bedfa58e199b8440df5d410c02fe4e2
Hekla Rún Ámundadóttir <heklaruna@gmail.com>
2016-10-24 11:00:33 +0000

    gerðum isWinner

--- src/main/java/is/ru/tictactoe/Board.java
+++ src/main/java/is/ru/tictactoe/Board.java
@@ -14,5 +14,48 @@ public class Board {
 			 }
 		 }
 	 }
-}
+	 public boolean isWinner(int row, int col){
+		 boolean winner = false;
+		 char player = board[row][col];
+		 int count = 0;
+		 
+		 //athugum larett
+		 for(int i : SIZE){
+			 if(board[i][col] == player)
+				 count++;
+		 }
+		 winner = win(count);
+		 
+		 //athugum lodrett
+		 for(int j : SIZE){
+			 if(board[row][j] == player)
+				 count++;
+		 }
+		 winner = win(count);
+		 
+		 //athugum a ska fra vinstri
+		 for(int i : SIZE){
+			 if(board[i][i] == player)
+				 count++
+		 }
+		 winner = win(count);
+		 
+		 //athugum a ska fra haegri
+		 for(int j : SIZE){
+			 if(board[j][2-j] == player)
+				 count++;
+		 }
+		 winner = win(count);
+		 
+		 return winner;
+		 
+	 }
+	 private boolean win(int count){
+		 boolean win = false;
+		 if(count == SIZE) {
+			 win = true;
+		 }
+		 count = 0;
+		 return win;
+	 }
 
