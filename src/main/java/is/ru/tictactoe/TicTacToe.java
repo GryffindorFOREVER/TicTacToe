6b030631825932fab167503e9354c932cbf36162
Tinna Dis Sigmarsdottir <tinna@i-1217c9a8.cs1004cloud.internal>
2016-10-24 00:32:02 +0000

    Dummy test class + dummy test case that passes

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -1,7 +1,15 @@
 package is.ru.tictactoe;
 
 public class TicTacToe {
+
+	private String yolo = "YOLO";
+	
+	public String YOLO() {
+
+		return yolo;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("YOLO");
-	}
+	}	
 }
---
+++ src/test/java/is/ru/tictactoetest/TicTacToeTest.java
@@ -0,0 +1,18 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TicTacToeTest { 
+
+    public static void main(String args[]) {
+    	org.junit.runner.JUnitCore.main("is.ru.tictactoetest.TicTacToeTest");
+    }
+
+   @Test
+   public void testYOLO() {
+	TicTacToe tic = new TicTacToe();
+	assertEquals("YOLO", tic.YOLO());	
+    }		
+}
