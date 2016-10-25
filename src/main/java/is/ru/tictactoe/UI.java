e7d15351682b5bfb973d7db263ecfbd10285f8c6
Kristin Vala Einarsdottir <kristine15@ru.is>
2016-10-25 16:39:58 +0000

    added player instructions

--- src/main/java/is/ru/tictactoe/UI.java
+++ src/main/java/is/ru/tictactoe/UI.java
@@ -14,10 +14,15 @@ public class UI {
 		for(int i = 0; i < 3; i++){
 			for(int j = 0; j < 3; j++){
 				System.out.print(b[i][j]);
-				System.out.print("jojo");
 			}
 			System.out.println();
 		}
+	}
+
+	public void play(){
+		displayBoard();
+		System.out.println("Choose a tile from 1 to 9");
+		System.out.println("Player " + gamePlay.getPlayer() + " make your move ");
 	}
 
 
