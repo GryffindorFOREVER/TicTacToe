9328e8e392e0e7c8d07d328716d7ccf5ead5d4b6
Hekla Rún Ámundadóttir <heklaruna@gmail.com>
2016-10-28 10:29:38 +0000

    gerði player1 og testaði hann

--- src/main/java/is/ru/tictactoe/Player.java
+++ src/main/java/is/ru/tictactoe/Player.java
@@ -1,6 +1,20 @@
 package is.ru.tictactoe;
 
 public class Player {
+	
+	private String player1;
+	
+	Player() {
+		player1 = null;
+	}
+	
+	public void setPlayer1(String player){
+		player1 = player;
+	}
+	
+	public String getPlayer1(){
+		return player1;
+	}
 
 
 }
--- src/test/java/is/ru/tictactoetest/PlayerTest.java
+++ src/test/java/is/ru/tictactoetest/PlayerTest.java
@@ -1,5 +1,15 @@
 package is.ru.tictactoe;
 
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class PlayerTest {
 
+	@Test
+	public void testPlayer1(){
+		Player p = new Player();
+		p.setPlayer1("anna");
+		assertEquals("anna", p.getPlayer1());
+	}
 }
